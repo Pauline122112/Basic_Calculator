c41ba012b53723b2213bedf8dfba4dccf9b31ed4
Pauline122112 <pauline.design007@gmail.com>
2021-06-26 16:11:41 -0500

    added calculator class

---
+++ index.js
@@ -0,0 +1,45 @@
+/*add class at the top of the table*/
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+}
+/*what can it perform?*/
+clear() {
+
+}
+
+delete() {
+
+}
+
+appendNummber(number) {
+
+}
+
+chooseOperation(operation) {
+
+}
+
+compute() {
+
+}
+
+updateDisplay() {
+
+}
+
+
+/*Constant variables which are all of the buttons */
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton =document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+const 
+
+/*now to figure out to know what number is typed where*/
+
